abe8224723dcd1b47f604e20721eb1bb8288f85b
krsaylo <krcsaylo@antiquespride.edu.ph>
2026-04-14 11:58:58 +0800

    Changes to repo

--- Lab_0/GRAELLOS_BS INFO 3B_LAB_0/Code_SetUp/hello.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello from Lab 0!");
--- Lab_0/GRAELLOS_BS INFO 3B_LAB_0/Code_SetUp/info.js
+++
@@ -1,7 +0,0 @@
-const name = "Paula";
-const course = "BS INFO 3B";
-const school = "UA Main";
-
-console.log("Student:", name);
-console.log("Course & Section:", course);
-console.log("School:", school);
